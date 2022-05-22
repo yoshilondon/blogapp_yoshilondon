1252196a9dbb86fa195c7d5c3c649be3e414ae64
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-05-22 09:08:35 +0100

    new 画面の作成→form表示→createアクション追加→flashメッセージ表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,24 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
-    end    
+    end 
+    
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to article_path(@article), notice: 'saved'
+        else
+            flash.now[:error] = 'Save failed'
+            render :new
+        end    
+    end
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
 end
