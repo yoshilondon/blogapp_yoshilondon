f3cb829767cf2736f5c9665d90885769004b52f0
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-05-22 20:00:41 +0100

    Pry-byebugを導入

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update]
 
-    def index
+    def index        
         @articles = Article.all
     end
 
