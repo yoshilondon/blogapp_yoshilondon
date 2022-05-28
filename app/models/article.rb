513f7922521dacb8162a4bbf2f431b165193275a
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-05-28 15:15:22 +0100

    validation uniquenessを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,10 +10,11 @@
 #
 class Article < ApplicationRecord
     validates :title, presence: true
-    validates :title, length: { minimum: 2, maximum: 100 }    
+    validates :title, length: { minimum: 2, maximum: 100 }
     
     validates :content, presence: true
     validates :content, length: { minimum: 10 }
+    validates :content, uniqueness: true
 
     def display_created_at
         I18n.l(self.created_at, format: :default)
